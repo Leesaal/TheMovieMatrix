52e8d40e7b4fbf5e1a6f02c9a6330bbad09dd2fc
SalmY001 <80605132+SalmY001@users.noreply.github.com>
2023-03-11 20:44:04 +0000

    add modal starter to project

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -215,5 +215,31 @@ var loadSearchHistory = function() {
     }
 };
 
+// Get the modal
+var modal = document.getElementById("myModal");
+var modalBtn = document.getElementById("searchBtn");
+var spanModal = document.getElementsByClassName("close")[0]; //close modal
+
+// When the user clicks the search button, open the modal if film name not found
+modalBtn.onclick = function() {
+    if (movieName == true) {
+        modal.style.display = "none";
+    } else { modal
+    }
+    
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanModal.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
