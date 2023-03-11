4d96a2cf3ccab0db849febee7ca57a04b09bf831
Carmela <carmela881@outlook.com>
2023-03-11 09:26:43 +0000

    added search button event listener

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,152 @@
-//Global Variables
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Global Variables
 
 
 // Event Listener when click on the button from the search div
@@ -42,48 +189,6 @@ function currentMovie (movieName) {
 
 
 
-
-// // Create function to amend user input into url friendly format
-
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '019a625d93msh4b8ca83c4e651e4p1b04f6jsn06afb2002bf9',
-// 		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
-// 	}
-// };
-
-// 	 movieName = "White Chicks",
-// movieName = movieName.trim(),
-// movieName = movieName.replace(" ", "%20"),
-
-// fetch("https://movie-database-alternative.p.rapidapi.com/?s=" + movieName + "&r=json&page=1", options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-
-// console.log(options);
-
-
-
-
-
-
-// // second movie database API
-
-// var secondAPIKey = "97c267f9a2d9d89d1419f2261423af96";
-
-// fetch("https://api.themoviedb.org/3/movie/"
-// + imdbID
-// + "?api_key="
-// + secondAPIKey
-// + "&language=en-US")
-// .then(response => response.json())
-// .then(response => console.log(response));
-
-// var posterURL = "https://image.tmdb.org/t/p/w500/";
-// var test = 0;
 
 
 
