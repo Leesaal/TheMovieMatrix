c39cd271ffccb70489ee4f8b35e09bd5b81426dc
Carmela <carmela881@outlook.com>
2023-03-11 12:41:24 +0000

    added searchHistory function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,7 +10,7 @@ var posterURL = "https://image.tmdb.org/t/p/w500/"
 
 
 
-
+// Added Event Listener to searchBtn
 searchBtn.addEventListener("click", function(event) {
 	event.preventDefault();
 // Options object witht the headers to access RapidAPI and Movie Database
@@ -19,9 +19,9 @@ const options = {
 	headers: {
 		'X-RapidAPI-Key': '019a625d93msh4b8ca83c4e651e4p1b04f6jsn06afb2002bf9',
 		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
-	}
-	
+	}	
 };
+
 
 // Create function to amend user input into url friendly format
 	var imdbIDKey;
@@ -29,6 +29,7 @@ const options = {
 	movieName = movieName.trim(); //Trim whitespaces from the query
 	movieName = movieName.replace(" ", "%20"); // Replaces spaces with %20
 	console.log(movieName);
+
 
 // First API call to The Movie Database for Title and imdbID
 
@@ -63,7 +64,7 @@ fetch("https://movie-database-alternative.p.rapidapi.com/?s="
 		+ "?api_key="
 		+ secondAPIKey
 		+ "&language=en-US")
-	// Parse the response to json
+// Parse the response to json
 		.then(function (response) {
 			return response.json();
 		  })
@@ -82,7 +83,7 @@ fetch("https://movie-database-alternative.p.rapidapi.com/?s="
 	moviePoster.setAttribute("src",posterURL+data.poster_path);
 
 
-	//If no movie entered or name of movie is incorrect, send alert (to be replaced by module) to enter a name of movie
+//If no movie entered or name of movie is incorrect, send alert (to be replaced by module) to enter a name of movie
 
 	if (movieName === "" || movieName === null) {
 		//send alert (to be replaced by module) if search is incorrect or empty
@@ -90,22 +91,59 @@ fetch("https://movie-database-alternative.p.rapidapi.com/?s="
 		event.preventDefault();
 		console.log("It works");
 	} else {
-		// If movieName is valid, display currentMovie 
+
+// If movieName is valid, display searchHistory function
 
 		searchHistory(movieName);
-		currentMovie(movieName);
-		console.log("it works");
+		console.log("It works");
 	}
 	return movieName;
 		
-	
 });
 });
-
 });
 
 
-
+function searchHistory(movieName) {
+	// Remove existing search history entries that contain the current movieName
+	document.querySelectorAll('.past-search[data-movie-name="' + movieName + '"]').forEach(function(elem) {
+	  elem.remove();
+	});
+  
+	// Create new search history entry
+	var searchHistoryEntry = document.createElement("p");
+	searchHistoryEntry.classList.add("past-search");
+	searchHistoryEntry.setAttribute("data-movie-name", movieName);
+	searchHistoryEntry.textContent = movieName;
+  
+	// Add the new search history entry to the search history list
+	var searchHistoryContainer = document.querySelector(".search-history");
+	searchHistoryContainer.appendChild(searchHistoryEntry);
+  
+	// Container for movie entry: create <div> element with a "past-search-container" class and append movie entry to the Container
+	var searchEntryContainer = document.createElement("div");
+	searchEntryContainer.classList.add("past-search-container");
+	searchEntryContainer.append(searchHistoryEntry);
+  
+	// Append Container for movie to Search History Container Element
+	var searchHistoryContainerEl = document.getElementById("search-history-container");
+	searchHistoryContainerEl.append(searchEntryContainer);
+  
+	// The current movieName is pushed onto the savedSearches array, and the updated array is stored in local storage
+	savedSearches.push(movieName);
+	localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+  
+	//Checks if there are any saved searches in local storage
+	if (savedSearches.length > 0){
+	  // If there are saved searches, parse them from a JSON string back into an array
+	  var previousSavedSearches = localStorage.getItem("savedSearches");
+	  savedSearches = JSON.parse(previousSavedSearches);
+	}
+  
+	// Clear search Query field
+	document.getElementById("searchQuery").value = "";
+  };
+  
 
 
 // Youtube API key and var for selecting div for videos
@@ -128,7 +166,7 @@ searchBtn.addEventListener("click", function(event) {
 })
 
 
-// create fetch using "movie trailer" in the query to specify youtube request
+// Create fetch using "movie trailer" in the query to specify youtube request
 
 function videoSearch(APIkey, search) {
 	fetch("https://www.googleapis.com/youtube/v3/search?key="
